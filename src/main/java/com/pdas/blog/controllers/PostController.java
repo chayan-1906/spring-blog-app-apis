dfc97e817654887fbfb9bd16f0b7d708c0dee065
Padmanabha Das <padmanabhadas9647@gmail.com>
2022-08-06 23:23:50 +0530

    upload image

--- src/main/java/com/pdas/blog/controllers/PostController.java
+++ src/main/java/com/pdas/blog/controllers/PostController.java
@@ -1,10 +1,18 @@
 package com.pdas.blog.controllers;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +22,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.pdas.blog.config.AppConstants;
 import com.pdas.blog.playloads.ApiResponse;
 import com.pdas.blog.playloads.PostDto;
 import com.pdas.blog.playloads.PostResponse;
+import com.pdas.blog.services.FileService;
 import com.pdas.blog.services.PostService;
 
 @RestController
@@ -27,6 +37,12 @@ public class PostController {
 
 	@Autowired
 	private PostService postService;
+
+	@Autowired
+	private FileService fileService;
+
+	@Value("${project.image}")
+	private String path;
 
 	// create
 	@PostMapping("/user/{userId}/category/{categoryId}/posts")
@@ -88,4 +104,25 @@ public class PostController {
 		List<PostDto> result = this.postService.searchPosts(keywords);
 		return new ResponseEntity<List<PostDto>>(result, HttpStatus.OK);
 	}
+
+	// post image upload
+	@PostMapping("/post/image/upload/{postId}")
+	public ResponseEntity<PostDto> uploadPostImage(@RequestParam("image") MultipartFile image,
+			@PathVariable Integer postId) throws IOException {
+		PostDto postById = this.postService.getPostById(postId);
+		String fileName = this.fileService.uploadImage(path, image);
+		postById.setImageName(fileName);
+		PostDto updatePost = this.postService.updatePost(postById, postId);
+		return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+	}
+
+	// method to serve files
+	@GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response)
+			throws IOException {
+		InputStream resource = this.fileService.getResource(path, imageName);
+		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+		StreamUtils.copy(resource, response.getOutputStream());
+	}
+
 }
