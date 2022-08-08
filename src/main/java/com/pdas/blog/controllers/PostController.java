6032941585466eb2478859b9789f5b8d2e7567ba
Padmanabha Das <padmanabhadas9647@gmail.com>
2022-08-08 17:44:30 +0530

    Post Image completed

--- src/main/java/com/pdas/blog/controllers/PostController.java
+++ src/main/java/com/pdas/blog/controllers/PostController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
-import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
